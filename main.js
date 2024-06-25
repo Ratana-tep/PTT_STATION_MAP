643a8c7a355cf4ed09fa06a541b68ba29e8835a4
Ratana-tep <tepratana010@gmail.com>
2024-06-25 10:35:16 +0700

    add

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/service-worker.js')
+//         .then((registration) => {
+//             console.log('Service Worker registered with scope:', registration.scope);
+//         }).catch((error) => {
+//             console.log('Service Worker registration failed:', error);
+//         });
+// }
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        }).catch((error) => {
-            console.log('Service Worker registration failed:', error);
-        });
-}
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/MAPTT_0114/service-worker.js').then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }).catch(function(error) {
+        console.log('Service Worker registration failed:', error);
+      });
+    });
+  }
--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,46 @@
-self.addEventListener('install', (event) => {
-    console.log('Service worker installing...');
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/MAPTT_0114/',
+//   '/MAPTT_0114/styles/main.css',
+  '/MAPTT_0114/main.js',
+  '/MAPTT_0114/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Service worker activating...');
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+// Activate event
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
