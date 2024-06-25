2e413e2d14c34889d7867f4138b09c43f1de7613
Ratana-tep <tepratana010@gmail.com>
2024-06-25 09:54:37 +0700

    add

--- pictures/Telegram Desktop/SQLQuery1.sql
+++
@@ -1,1 +0,0 @@
-SELECT [MAT_ID],[QTY],[PRICE],[VALUE_DETAIL],[POS_ID],[SHIFT_ID],[PAYMENT_TYPE_NAME],[Completed_TS],[BUS_DATE] FROM [POSDB].[dbo].[RPT_BOR018] WHERE Completed_TS >= 'StartDate' AND Completed_TS <= 'ToDate';
